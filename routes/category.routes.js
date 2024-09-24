6c8f5c02ec90a9d11c26f10f30cf477d31fdfba5
Gourav Kumar <gouravkrd2131@gmail.com>
2024-09-24 07:53:28 +0530

    update

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -9,20 +9,18 @@ const catReqValidator = require("../middlewares/categoryReqValidator");
 module.exports = (app)=>{
     app.post("/ecomm/api/v1/auth/categories",[authMW.verifyToken, authMW.isAdmin ], CategoryController.createNewCategory);
 
-
-
     // localhost:8888/ecomm/api/v1/auth/categories/:name
-      app.get("/ecomm/api/v1/auth/categories/:name", CategoryController.findCategorybyname );
+    app.get("/ecomm/api/v1/auth/categories/:name", CategoryController.findCategorybyname );
 
      //localhost:8888/ecomm/api/v1/auth/categories
-     app.get("/ecomm/api/v1/auth/categories", CategoryController.findAll);
+    app.get("/ecomm/api/v1/auth/categories", CategoryController.findAll);
     
     
     // localhost:8888/ecomm/api/v1/auth/categories/:id
     app.get("/ecomm/api/v1/auth/categories/:id", CategoryController.findCategorybyId );
 
     // localhost:8888/ecomm/api/v1/auth/categories/update
-    app.put("/ecomm/api/v1/auth/categories/update",[ catReqValidator.requestValidator, authMW.verifyToken , authMW.isAdmin], CategoryController.updateCategory)
+    app.put("/ecomm/api/v1/auth/categories/update",[ catReqValidator.requestValidator, authMW.verifyToken , authMW.isAdmin], CategoryController.updateCategory);
 
     // localhost:8888/ecomm/api/v1/auth/categories/delete
     app.delete("/ecomm/api/v1/auth/categories/delete",[ authMW.verifyToken , authMW.isAdmin], CategoryController.deleteCategorybyname);
